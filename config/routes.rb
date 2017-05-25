77f176bafbb0828395ceaad5c95f8cd79a064728
khajamainoddin <khajamainoddin.mohd@gmail.com>
2017-05-25 23:12:02 +0530

    Implemented sign up.

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,12 @@
 Rails.application.routes.draw do
+  
+
 	root "projects#index"
 
 	resources :projects do
      resources :tickets
    end
+
+   resources :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
