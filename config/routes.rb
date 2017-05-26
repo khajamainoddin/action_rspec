0361c3e4f6983bf928d67b6ee58a154b67fccc5f
khajamainoddin <khajamainoddin.mohd@gmail.com>
2017-05-26 10:33:40 +0530

    Implemented signing in.

--- config/routes.rb
+++ config/routes.rb
@@ -8,5 +8,8 @@ Rails.application.routes.draw do
    end
 
    resources :users
+
+   get "/signin", to: "sessions#new"
+   post "/signin", to: "sessions#create"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
